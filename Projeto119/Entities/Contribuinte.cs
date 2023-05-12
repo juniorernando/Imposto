4d09e680edf794f5d5237b8f11e2a621b8d07678
Junior Fernando <junior_618@hotmail.com>
2023-05-12 18:32:44 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto119/Entities/Contribuinte.cs
@@ -0,0 +1,20 @@
+﻿namespace Projeto119.Entities
+{
+    abstract class TaxPayer
+    {
+        public string Name { get; set; }
+        public double AnualIncome { get; set; }
+
+        public TaxPayer()
+        {
+        }
+
+        protected TaxPayer(string name, double anualIncome)
+        {
+            Name = name;
+            AnualIncome = anualIncome;
+        }
+
+        public abstract double Tax();
+    }
+}
